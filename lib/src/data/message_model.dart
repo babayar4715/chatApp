1b7a2cdc599a957e915d3ab7ac1c905f824b2ef6
babayar4715 <babayarkenzhebekov@gmail.com>
2023-03-29 22:16:33 +0300

    chat app full

--- lib/src/data/message_model.dart
+++ lib/src/data/message_model.dart
@@ -2,17 +2,23 @@
 import 'dart:convert';
 
 class Message {
-  const Message({
+  Message({
+    required this.dateTime,
     required this.sender,
     required this.sms,
+    this.isMy,
   });
   final String sender;
   final String sms;
+  final DateTime dateTime;
+  bool? isMy;
 
   Map<String, dynamic> toJson() {
     return <String, dynamic>{
       'sender': sender,
       'sms': sms,
+      'dateTime': dateTime.millisecondsSinceEpoch,
+      'isMy': isMy,
     };
   }
 
@@ -20,7 +26,8 @@ class Message {
     return Message(
       sender: map['sender'] as String,
       sms: map['sms'] as String,
+      dateTime: DateTime.fromMillisecondsSinceEpoch(map['dateTime'] as int),
+      isMy: map['isMy'] != null ? map['isMy'] as bool : null,
     );
   }
 }
-// dart data class generation
